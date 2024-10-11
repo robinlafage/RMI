cf2541893c6c7103e2b91f3191bbed1dd8736db1
Valentin RENIER <valentin.renier@etu.esisar.grenoble-inp.fr>
2024-10-11 15:53:41 +0100

    Avancées dans le chall 3, récupération et stockage des balises OK

--- pClient/challenge3.py
+++ pClient/challenge3.py
@@ -6,11 +6,15 @@ import xml.etree.ElementTree as ET
 
 CELLROWS=7
 CELLCOLS=14
+SPEED = 0.15
+OFFSET = 0.04
 
 class MyRob(CRobLinkAngs):
-    def __init__(self, rob_name, rob_id, angles, host, previous_distances):
+    def __init__(self, rob_name, rob_id, angles, host, beacons_positions, start_positions, visiting_beacon):
         CRobLinkAngs.__init__(self, rob_name, rob_id, angles, host)
-        self.previous_distances  = previous_distances
+        self.beacons_positions  = beacons_positions
+        self.start_positions = start_positions
+        self.visiting_beacon = visiting_beacon
 
     # In this map the center of cell (i,j), (i in 0..6, j in 0..13) is mapped to labMap[i*2][j*2].
     # to know if there is a wall on top of cell(i,j) (i in 0..5), check if the value of labMap[i*2+1][j*2] is space or not
@@ -65,48 +69,124 @@ class MyRob(CRobLinkAngs):
             
 
     def wander(self):
+        if self.measures.ground <= 0 : 
+            for index, beacon in enumerate(self.visiting_beacon) :
+                if beacon == True :
+                    self.visiting_beacon[index] = False
+        self.beaconsManagement()
+        self.motorsCommand()
+        print(str(self.beacons_positions))
+        
+        
+    def motorsCommand(self) : 
         center_id = 0
         left_id = 1
         right_id = 2
         back_id = 3
+
+        if self.measures.irSensor[center_id] > 2 :
+            if self.measures.irSensor[left_id] > self.measures.irSensor[right_id] :
+                self.driveMotors(0.15,-0.15)
+            elif self.measures.irSensor[right_id] > self.measures.irSensor[left_id] :
+                self.driveMotors(-0.15,0.15)
+        elif self.measures.irSensor[left_id] > 8 :
+            self.driveMotors(0.15, 0.0)
+        elif self.measures.irSensor[right_id] > 8 :
+            self.driveMotors(0.0,0.15)
+        else : 
+            self.goForward(self.measures.compass)
+    
+    def beaconsManagement(self):
+        #Initializing the beacons_position dict
+        if self.beacons_positions == {}:
+            beacon=0
+            while beacon < int(self.nBeacons) :
+                self.beacons_positions[beacon] = []
+                self.visiting_beacon.append(False)
+                beacon += 1
+
+        #Registering beacons in beacons_positions
+        beacon = 0 
+        for beacon in range(int(self.nBeacons)) :
+            if self.visiting_beacon[beacon]==False and self.beacons_positions[beacon] != [] :
+                if isinstance(self.beacons_positions[beacon][0], list) : 
+                    self.beacons_positions[beacon] = self.averagePosition(self.beacons_positions[beacon])
+
+        #If we are on start, we set the start_points here 
+        if self.measures.ground == 0 :
+            if self.start_positions == []:
+                self.start_positions = [self.measures.x, self.measures.y]
+                self.beacons_positions[0] = [0,0]
+            print("We are on start point !")
         
-        # These statements make the robot rotate when there is a wall in front
-        if    self.measures.irSensor[center_id]  > 1.1\
-            and self.measures.irSensor[right_id] > self.measures.irSensor[left_id]\
-            and self.measures.irSensor[right_id] > 1.15:
-            self.driveMotors(-0.15,+0.15)
-        elif    self.measures.irSensor[center_id]  > 1.1\
-            and self.measures.irSensor[left_id]   > self.measures.irSensor[right_id]\
-            and self.measures.irSensor[left_id] > 1.15:
-            self.driveMotors(+0.15,-0.15)
+        #We register the positions relatives to the start_points until we get out of the beacon 
+        elif self.measures.ground > 0 : 
+            print("Visiting a beacon !")
+            beacon = self.measures.ground
+            self.registerBeaconPositions(beacon)
+            self.visiting_beacon[beacon] = True
 
-        # Security statements, to avoid collision with a lateral wall
-        elif self.measures.irSensor[left_id]> 15.0\
-            and self.measures.irSensor[left_id]   > self.measures.irSensor[right_id]:
-            self.driveMotors(0.15,-0.05)
-        elif self.measures.irSensor[right_id]> 15.0\
-            and self.measures.irSensor[right_id]   > self.measures.irSensor[left_id]:
-            self.driveMotors(-0.05,0.15)
+
+    def registerBeaconPositions(self, beacon):
+        alreadyWriten=True
+        try :
+            if isinstance(self.beacons_positions[beacon][0], list):
+                alreadyWriten=False
+        except : 
+            alreadyWriten = False
+        if alreadyWriten == False :
+            if not self.start_positions == []:
+                self.beacons_positions[beacon].append([self.measures.x - self.start_positions[0], self.measures.y - self.start_positions[1]])
+            else : 
+                print("FIRST BEACON NOT SET, PLEASE START AT FIRST BEACON")   
+
+
+    def averagePosition(self, positions_list):
+        xTotal = 0
+        yTotal = 0
+        index = 0
+        for position in positions_list :
+            xTotal += position[0]
+            yTotal += position[1]
+            index += 1
+        return self.roundPositions([xTotal / index, yTotal / index])
+
+    def roundPositions(self, positions_coordinates) :
+        return_list = []
+        for number in positions_coordinates :
+            if int(abs(number)+0.5) > int(number) :
+                if number > 0 :
+                    return_list.append(int(number+0.5))
+                else : 
+                    return_list.append(int(number-0.5))
+            else : 
+                return_list.append(int(number))
+        return return_list
+    
+    def goForward(self, dir):
+        #If we are not deviating from the direction, we keep going
+        # Un degré de tolérance, surement a supprimer #if (dir >= -1 and dir <= 1) or (dir >= 89 and dir <= 91) or (dir >= 179 and dir <= 180) or (dir <= -180 and dir >= -181) or (dir >= -91 and dir <= -89):
+        if dir == 0 or dir == 90 or dir == -90 or dir == 180 or dir == -180:
+            self.driveMotors(SPEED, SPEED)
+            return
         
-        # Security statements, to avoid collision with a front wall
-        elif self.measures.irSensor[center_id]> 15.0\
-            and self.measures.irSensor[left_id]   > self.measures.irSensor[right_id]:
-            self.driveMotors(0.15,-0.15)
-        elif self.measures.irSensor[center_id]> 15.0\
-            and self.measures.irSensor[right_id]   > self.measures.irSensor[left_id]:
-            self.driveMotors(-0.15,0.15)
+        #Else, we adjust the direction
+
+        if (dir <= 45 and dir > 0) or (dir <= 135 and dir > 90) or (dir <= -135 and dir > -180) or (dir <= -45 and dir > -90):
+            #Turn slowly right
+            # print("Adjusting direction - turning right")
+            self.driveMotors(SPEED, SPEED-OFFSET)
+
+        elif (dir >= -45 and dir < 0) or (dir >= 45 and dir < 90) or (dir >= 135 and dir < 180 and dir > 0) or (dir >= -135 and dir < -90):
+            #Turn slowly left
+            # print("Adjusting direction - turning left")
+            self.driveMotors(SPEED-OFFSET, SPEED)
+
+    def previousPositionsStack(self):
+        #TODO Implement a stack to memorize the previous positions of the robot
+        pass
 
 
-        else:
-            print('Go')
-            self.driveMotors(-0.15,-0.15)
-        
-        # Save the previous distances in order to permit to make the robot go straight
-        self.previous_distances=[self.measures.irSensor[center_id],self.measures.irSensor[left_id],self.measures.irSensor[right_id],self.measures.irSensor[back_id]]
-
-        print(str(self.measures.beacon))
-
-        
 class Map():
     def __init__(self, filename):
         tree = ET.parse(filename)
@@ -154,7 +234,7 @@ for i in range(1, len(sys.argv),2):
         quit()
 
 if __name__ == '__main__':
-    rob=MyRob(rob_name,pos,[180.0,-120.0,120.0,0.0],host, [])
+    rob=MyRob(rob_name,pos,[0.0,90.0,-90.0,180.0],host,{}, [], [])
     if mapc != None:
         rob.setMap(mapc.labMap)
         rob.printMap()
